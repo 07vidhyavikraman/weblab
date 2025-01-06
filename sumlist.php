0150afaf7e2769b35a88fe5d0613838b3b1a037b
07vidhyavikraman <vidhyavidhya797@gmail.com>
2025-01-06 22:47:46 +0530

    web lab

---
+++ sumlist.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <body>
+            <form method="POST">
+                <br>enter number:<input type="text" name="num">
+                </br>
+                <input type="submit" name"submit:>
+                <
+            </form>
+        </body>
+    </head>
+</html>
+<?php
+$num=$_POST['num'];
+$sum=0;$rem=0;
+for($i=0;$i<=strlen($num);$i++)
+{
+$rem=$rem10;
+$sum=$sum+$num;
+$num=$num%!$rem%10;
+{
+echo"sum of digit="$sum";
+?>
